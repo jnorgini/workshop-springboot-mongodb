2a9c6a920ec0e22d415d1d9793017009a2ac227e
jnorgini <jnorgini@gmail.com>
2023-01-30 01:38:18 -0300

    Obtendo um usuario por id

--- src/main/java/com/juliananorgini/workshopmongo/resources/UserResource.java
+++ src/main/java/com/juliananorgini/workshopmongo/resources/UserResource.java
@@ -5,8 +5,9 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.juliananorgini.workshopmongo.domain.User;
@@ -20,10 +21,16 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 	
-	@RequestMapping(method=RequestMethod.GET)
+	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 }
--- src/main/java/com/juliananorgini/workshopmongo/services/UserService.java
+++ src/main/java/com/juliananorgini/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.juliananorgini.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.juliananorgini.workshopmongo.domain.User;
 import com.juliananorgini.workshopmongo.repository.UserRepository;
+import com.juliananorgini.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
