4f70f93f143c3031b3ef46a8df5a3406c76a92e2
jnorgini <jnorgini@gmail.com>
2023-01-30 19:14:11 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/juliananorgini/workshopmongo/resources/UserResource.java
+++ src/main/java/com/juliananorgini/workshopmongo/resources/UserResource.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -53,4 +52,12 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.FromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
 }
